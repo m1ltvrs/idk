9f8a1b8f0a98cbbddb55445e6e4e7b2d17313dea
voucan <vcn4313@gmail.com>
2026-04-10 20:53:36 -0400

    Update notify.js

--- storage/js/notify.js
+++ storage/js/notify.js
@@ -41,7 +41,7 @@ function createNotification(message) {
     notification.appendChild(header);
 
     const messageSpan = document.createElement('div');
-    messageSpan.innerHTML = `US4 is discontinued. We recommend you export your data to US5. For help on exporting your data and getting to US5 go to <a href="${window.origin}/us5.html" target="_blank" style="color:#4da6ff; text-decoration:underline;">this page</a>.`;
+    messageSpan.innerHTML = ` US4 is discontinued. We have fixed US5 and are rebuilding with new links. Go to <a href="${window.origin}/us5.html" target="_blank" style="color:#4da6ff; text-decoration:underline;">this page</a> to export your data and find a US5 link.`;
     messageSpan.style.marginBottom = '20px';
     notification.appendChild(messageSpan);
 
